33a7e65fcaa03846e6db217da5f3d31bc1378777
okidoi <okidoi@gmail.com>
2021-02-10 23:37:47 -0300

    primeiro exemplo de typescript

---
+++ typescript/app.ts
@@ -0,0 +1,15 @@
+let message = "Help me, Obi-Wan Kenobi. You're my only hope"
+console.log(message)
+
+let episode: number = 4
+//episode = '4' // Por padrao ele assume o comportamento do javascript e torna string. assim ele concatenara em caso de um + adiante
+
+console.log("This is episode " + 4)  
+
+episode = episode + 1
+console.log("This is episode " + episode)  
+
+let nome: any
+nome = 'Marcio'
+nome = 8
+console.log("o nome e: " + nome)  //any aceita mudar igual a var em javascript
---
+++ typescript/dist/app.js
@@ -0,0 +1,11 @@
+var message = "Help me, Obi-Wan Kenobi. You're my only hope";
+console.log(message);
+var episode = 4;
+//episode = '4' // Por padrao ele assume o comportamento do javascript e torna string. assim ele concatenara em caso de um + adiante
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("This is episode " + episode);
+var nome;
+nome = 'Marcio';
+nome = 8;
+console.log("o nome e: " + nome); //any aceita mudar igual a var em javascript
